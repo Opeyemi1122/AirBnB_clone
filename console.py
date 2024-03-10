14083ffc72daa26e069097a07d6921afcc45b567
Opeyemi1122 <alabiopex@gmail.com>
2024-03-10 01:22:46 +0100

    console created

--- console.py
+++ console.py
@@ -1,7 +1,28 @@
 #!/usr/bin/python3
 """Defines the HBnB console."""
+
 import cmd
-import re
-from shlex import split
-from models import storage
-from models.base_model import BaseModel
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print("")  # To print a newline before exiting
+        return True
+
+    def do_help(self, arg):
+        """Help command to display help information"""
+        super().do_help(arg)
+
+    def emptyline(self):
+        """Do nothing on an empty line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
